5fbc9f22e765cfff5094d0aa62c6026b5c15b2db
amal masghouni <masghouniamal84@gmail.com>
2025-07-18 16:25:17 +0100

    pp

--- src/main/java/com/example/Foyer/DAO/Entities/Universite.java
+++ src/main/java/com/example/Foyer/DAO/Entities/Universite.java
@@ -21,12 +21,6 @@ public class Universite implements Serializable {
     long idUniversite;
     String nomUniversite;
     String adresse;
-//PUSH MAIN
-    //TESTTT
-    //
-    //hhh
-    //bbb
-    //hhh
-    //lll
+
 
 }
